4fea6e5fe0be27ba22cc1d907b8450762a36fd35
Mexxiax <miguelsoares91@gmail.com>
2022-04-12 19:16:26 +0100

    stock exceptions

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,19 @@
 class StocksController < ApplicationController
 
     def search
+        return invalid_symbol unless params[:stock].present?
+
         @stock = Stock.new_lookup(params[:stock])
-        render 'users/my_portfolio'
+        return render 'users/my_portfolio' if @stock
+        invalid_symbol
     end
+
+    private
+
+    def invalid_symbol 
+        flash[:alert] = "Please enter a valid symbol to search"
+        redirect_to my_portfolio_path
+    end
+
 
 end
