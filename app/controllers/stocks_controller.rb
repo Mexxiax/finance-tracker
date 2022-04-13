af5eccace058790f2ae8e9f0690a579bf8de8db3
Mexxiax <miguelsoares91@gmail.com>
2022-04-13 18:40:47 +0100

    js.erb file added with ajax call

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,23 @@
 class StocksController < ApplicationController
 
-    def search
-        return invalid_symbol unless params[:stock].present?
-
-        @stock = Stock.new_lookup(params[:stock])
-        return render 'users/my_portfolio' if @stock
-        invalid_symbol
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result' }
+        end
+      end    
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result' }
+      end
     end
-
-    private
-
-    def invalid_symbol 
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
-    end
-
-
+  end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -11,5 +11,4 @@ class Stock < ApplicationRecord
         return nil
     end
   end
-
 end
